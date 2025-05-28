a291b9cd73f658ae6ad1618bdb2831559982eb2a
Dhari <dhari2049@hotmail.com>
2025-05-28 05:09:39 +0100

    added textarea and radio input

--- Rawr-Website/src/components/Form.jsx
+++ Rawr-Website/src/components/Form.jsx
@@ -8,6 +8,8 @@ function handleSubmit(event) {
     const formData = new FormData(formEl)
     console.log(formData.get("email"))
     console.log(formData.get("password"))
+    console.log(formData.get("Super Sayian Status"))
+    console.log(formData.get("msg"))
     formEl.reset()
 
 }
@@ -21,6 +23,7 @@ export default function Form() {
                 <form className="form-inputs" method="POST" onSubmit={handleSubmit}>
                     <label> Email:
                         <input
+                            id="email"
                             type="email"
                             name="email"
                             placeholder="example@hotmail.com"
@@ -29,11 +32,41 @@ export default function Form() {
 
                     <label> Password:
                         <input
+                            id="password"
                             type="password"
                             name="password"
                             placeholder="********"
                         />
                     </label>
+                    <label>
+                        Message:
+                        <textarea
+                            id="msg"
+                            name="msg"
+                            placeholder="i want to contact about.." > </textarea>
+                    </label>
+
+                    <fieldset className="fieldset-radio">
+                        <legend>Super Sayian Status</legend>
+                        <label>
+
+                            <input
+                                type="radio"
+                                name="Super Sayian Status"
+                                value="Super Sayian" />
+                            Super Sayian
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                name="Super Sayian Status"
+                                value="Super Sayian 2" />
+                            Super Sayian 2
+                        </label>
+                    </fieldset>
+
+
+
                     <button > Submit</button>
                 </form>
             </section>
