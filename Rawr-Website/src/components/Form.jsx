d85285f393bd2d06770ea218b355664267e3c876
Dhari <dhari2049@hotmail.com>
2025-07-13 21:03:35 +0100

    ...

--- Rawr-Website/src/components/Form.jsx
+++ Rawr-Website/src/components/Form.jsx
@@ -106,11 +106,7 @@ export default function Form() {
                             <option value="Blue"> Blue</option>
                         </select>
                     </label>
-                
-
-
-
-
+            
 
 
                     <button > Submit</button>
